f6e3a74ad8dd2b840da6f28ca4d4ff01ec39387d
anuroopkanna <anuroopindia16@gmail.com>
2023-04-01 23:58:31 +0530

    added:uc2

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package com.BridgeLabzDay15HashTable;
 
-public class HashMain {
+public class Main {
     public static void main(String[] args) {
         System.out.println("***** Welcome to the HastTableBST *****");
     }
