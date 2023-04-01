b179488054c86834917d7aab49d2da332f5faa2e
anuroopkanna <anuroopindia16@gmail.com>
2023-04-01 21:36:31 +0530

    added:uc1

--- HashNode.java
+++ HashNode.java
@@ -5,4 +5,17 @@ public class HashNode<K,V> {
 
     V value;
     HashNode<K,V> next;
+    //uc1
+    public HashNode(K key,V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node: " + " K= ").append(key).append(", V= ").append(value).append("\n");
+        if (next != null)
+            nodeString.append(" , ").append(next);
+        return nodeString.toString();
+    }
 }
